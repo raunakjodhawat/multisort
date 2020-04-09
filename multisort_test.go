5ef5e001d7cf9ab05edb27567dc177e6d92c2015
Raunak Jodhawat <raunakjodhawat@gmail.com>
2020-04-09 12:06:26 -0400

    Add documentation for tests, add examples, remove typo

--- multisort_test.go
+++ multisort_test.go
@@ -1,3 +1,4 @@
+// Package MultiSort provides gives the ability to sort a slice on the basis of multiple keys
 package multisort
 
 import (
@@ -5,35 +6,40 @@ import (
 	"testing"
 )
 
-type multiSortExamlePerson struct {
+// multiSortExamplePerson type, is used as an example type. It has two field, Name and Age
+type multiSortExamplePerson struct {
 	Name string
 	Age  int
 }
 
-func makeInput() []multiSortExamlePerson {
-	p1 := multiSortExamlePerson{
+// makeInput Function is a utility function, used by the Examples function to remove code redundancy
+// it creates a slice of type multiSortExamplePerson, again multiSortExamplePerson here is used as a Generic type T
+func makeInput() []multiSortExamplePerson {
+	p1 := multiSortExamplePerson{
 		Name: "Joe",
 		Age:  26,
 	}
-	p2 := multiSortExamlePerson{
+	p2 := multiSortExamplePerson{
 		Name: "Azin",
 		Age:  14,
 	}
-	p3 := multiSortExamlePerson{
+	p3 := multiSortExamplePerson{
 		Name: "Bold",
 		Age:  11,
 	}
-	p4 := multiSortExamlePerson{
+	p4 := multiSortExamplePerson{
 		Name: "AAND",
 		Age:  14,
 	}
-	multisortExamplePersons := []multiSortExamlePerson{p1, p2, p3, p4}
+	multisortExamplePersons := []multiSortExamplePerson{p1, p2, p3, p4}
 
 	return multisortExamplePersons
 }
 
-// Examples
-func ExampleMultiSort_MultiKey() {
+// ExampleMultiSort_multiKey Function, sorts the input with two keys
+// Sorts slice of multiSortExamplePerson with key: Name in Descending Order and then with Key: Age in Ascending
+// Prints the output, after converting the []interface back to expected(required) type
+func ExampleMultiSort_multiKey() {
 	multisortExamplePersons := makeInput()
 	sortKeys := []string{"Name", "Age"}
 	ascendingOrder := []bool{false, true}
@@ -42,12 +48,16 @@ func ExampleMultiSort_MultiKey() {
 		return
 	}
 	for i := range multiSortResponse {
-		multiSortResponse[i] = multiSortResponse[i].(multiSortExamlePerson)
+		multiSortResponse[i] = multiSortResponse[i].(multiSortExamplePerson)
 	}
 	fmt.Println(multiSortResponse)
 	// Output: [{Joe 26} {Azin 14} {AAND 14} {Bold 11}]
 }
-func ExampleMultiSort_SingleKeyType_1() {
+
+// ExampleMultiSort_singleKey1 Function, sorts the input with one key
+// Sorts slice of multiSortExamplePerson with key: Name in Ascending Order
+// Prints the output, after converting the []interface back to expected(required) type
+func ExampleMultiSort_singleKey1() {
 	multisortExamplePersons := makeInput()
 	sortKeys := []string{"Name"}
 	ascendingOrder := []bool{true}
@@ -56,13 +66,17 @@ func ExampleMultiSort_SingleKeyType_1() {
 		return
 	}
 	for i := range multiSortResponse {
-		multiSortResponse[i] = multiSortResponse[i].(multiSortExamlePerson)
+		multiSortResponse[i] = multiSortResponse[i].(multiSortExamplePerson)
 	}
 	fmt.Println(multiSortResponse)
 	// Output: [{Joe 26} {Bold 11} {Azin 14} {AAND 14}]
 }
 
-func ExampleMultiSort_SingleKeyType_2() {
+// ExampleMultiSort_singleKey2 Function, sorts the input with one key
+// Demonstrates that the default ascending order is applied, when nothing is provided with ascendingOrder slice input
+// Sorts slice of multiSortExamplePerson with key: Age in Ascending Order (Taken as default)
+// Prints the output, after converting the []interface back to expected(required) type
+func ExampleMultiSort_singleKey2() {
 	multisortExamplePersons := makeInput()
 	sortKeys := []string{"Age"}
 	var ascendingOrder []bool
@@ -71,31 +85,33 @@ func ExampleMultiSort_SingleKeyType_2() {
 		return
 	}
 	for i := range multiSortResponse {
-		multiSortResponse[i] = multiSortResponse[i].(multiSortExamlePerson)
+		multiSortResponse[i] = multiSortResponse[i].(multiSortExamplePerson)
 	}
 	fmt.Println(multiSortResponse)
 	// Output: [{Joe 26} {Azin 14} {AAND 14} {Bold 11}]
 }
 
-// tests
+// TestMultiSort, performs table testing with All possible combination of 2 keys and 2 orders. Taking each key one at a time and in combination
 func TestMultiSort(t *testing.T) {
-	p1 := multiSortExamlePerson{
+	// variable declaration for Type multiSortExamplePerson to be tested as a slice
+	p1 := multiSortExamplePerson{
 		Name: "Joe",
 		Age:  26,
 	}
-	p2 := multiSortExamlePerson{
+	p2 := multiSortExamplePerson{
 		Name: "Azin",
 		Age:  14,
 	}
-	p3 := multiSortExamlePerson{
+	p3 := multiSortExamplePerson{
 		Name: "Bold",
 		Age:  11,
 	}
-	p4 := multiSortExamlePerson{
+	p4 := multiSortExamplePerson{
 		Name: "AAND",
 		Age:  14,
 	}
 
+	// Input matrix for keys to be sorted with
 	sortKeysmatrix := [][]string{
 		{"Name"},
 		{"Name", "Age"},
@@ -106,6 +122,8 @@ func TestMultiSort(t *testing.T) {
 		{"Name", "Age"},
 		{"Name", "Age"},
 	}
+
+	// Input matrix for the order in which the slice must be sorted
 	ascendingOrderSlice := [][]bool{
 		{true},
 		{true, true},
@@ -116,7 +134,9 @@ func TestMultiSort(t *testing.T) {
 		{true, false},
 		{false, true},
 	}
-	expectedResults := [][]multiSortExamlePerson{
+
+	// Expected results based on above mentioned key and keyOrder combination
+	expectedResults := [][]multiSortExamplePerson{
 		{p4, p2, p3, p1},
 		{p1, p4, p2, p3},
 		{p1, p2, p4, p3},
@@ -126,18 +146,22 @@ func TestMultiSort(t *testing.T) {
 		{p1, p2, p4, p3},
 		{p1, p2, p4, p3},
 	}
+
+	// For all the expectedResults
 	for i, v := range expectedResults {
+		// create the input
 		multisortExamplePersons := makeInput()
+		// send it to the Multi Sort, to sort
 		multiSortResponse, err := MultiSort(multisortExamplePersons, sortKeysmatrix[i], ascendingOrderSlice[i])
 		if err != nil {
 			return
 		}
-		a := fmt.Sprint(v)
-		b := fmt.Sprint(multiSortResponse)
-		if a != b {
-			t.Errorf("Expected: %v Got %v", a, b)
+		// Convert the result and expectedResult to a string for easier comparison without the need of reflect package
+		expectedResult := fmt.Sprint(v)
+		multiSortResult := fmt.Sprint(multiSortResponse)
+		// Check for the match, if the expectedResult does not match multiSortResult, fail the test
+		if expectedResult != multiSortResult {
+			t.Errorf("Expected: %v Got %v", expectedResult, multiSortResult)
 		}
-		fmt.Println(v, multiSortResponse, a == b)
-
 	}
 }
